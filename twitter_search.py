029569c3ebfdc5ab31e2261118146d5719e37269
mary-keenan <mary.keenan@students.olin.edu>
2016-04-19 16:11:13 -0400

    runs search multiple times

--- twitter_search.py
+++ twitter_search.py
@@ -11,32 +11,42 @@ api = twitter.Api(consumer_key = consumer_key,
 
 def geo_collect_tweets(search_term,latitude,longitude,radius):
     """search for tweets within certain radius of latitude and longitude with certain keyword in them.
-        Returns the list of tweet texts
+        Returns the list of unique tweet texts
     """
+    i = None
     tweets = []
-    results = api.GetSearch(term = search_term, count=100, result_type = 'recent', geocode=(latitude, longitude, radius))
-    for tweet in results:
-        tweets.append(tweet.text)
-    return tweets 
+    rep = 1
+    for n in range(10): #can only search 100 tweets at a time, so run search multiple times
+    	results = api.GetSearch(term = search_term, 
+    		count = 100, 
+    		result_type = 'recent', 
+    		max_id = i, #start a search from the most recent tweet id, working backwards
+    		geocode =(latitude, longitude, radius))
+        for tweet in results:
+            tweets.append(tweet.text)
+        i = tweet.id - 1 #want it to start at the tweet after the last tweet
+        rep += 1
+    print len(set(tweets))
+    return list(set(tweets)) #set gets rid of repititve tweets, but need to return a list
 
 def geo_data_analysis(search_term):
     """analyzes the sentiment of tweets and return the average value for each region 
-        in dictionary form. Keys are 'NE' (northeast), 'S'(south), 'MW' (midwest),and  'W' (West)
     """
     map_pol = dict()
 
-    #A list of tweet text from each region
+    #A list of tweet texts from each region
     NE_text = geo_collect_tweets(search_term,42.781158,-71.398729,'250mi')
     S_text = geo_collect_tweets(search_term,33.000000,-84.000000,'500mi')
     MW_text = geo_collect_tweets(search_term,40.000000,-100.000000,'1000mi')
     W_text = geo_collect_tweets(search_term,35.000000,-120.000000,'250mi')
    
-    #A list of sentiment values from each region 
+    #A list of sentiment values for the tweets from each region 
     NE_sentiment_values = sentiment(NE_text)
     S_sentiment_values = sentiment(S_text)
     MW_sentiment_values = sentiment(MW_text)
     W_sentiment_values = sentiment(W_text)
 
+    #find the average sentiment value for each region
     NE_avg = sum(NE_sentiment_values)/len(NE_sentiment_values)
     S_avg = sum(S_sentiment_values)/len(S_sentiment_values)
     MW_avg = sum(MW_sentiment_values)/len(MW_sentiment_values)
@@ -46,8 +56,5 @@ def geo_data_analysis(search_term):
     print 'S_avg',S_avg
     print 'MW_avg',MW_avg
     print 'W_avg',W_avg
-
-search_term = '#mondaymotivation'
-count = 100
 
 geo_data_analysis('#bernie')
